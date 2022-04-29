9e184d14a8f06d2b3ee2e253ef5fa85ef6ee1d4d
paginesher <paginesher@gmail.com>
2022-04-29 14:19:59 -0400

    I hope I did ok

---
+++ Friday-ES6.js
@@ -0,0 +1,22 @@
+// /1 Convert the following function into an arrow function named "arrowMyFunction".
+// const myFunction = function (num1, num2) {
+//       return num1 + num2;
+// };
+
+const arrowMyFunction =(num) => num + num 
+console.log(num())
+
+//2 Convert the following function into a one-lined arrow function without a return statement (the return is implicit) . Name the function arrowAdd100
+// const add100 = function (money) {
+//     return money + 100;
+// };
+const arrowAdd100 = (money) => +100
+
+//3 Create an arrow function that takes TWO arguments, a string and a single letter.  The function will count the number of times the specified letter occurs in the string. **Not case sensitive
+// Examples:
+countLetters('Hello my name is John', 'i') //returns 1
+countLetters('Hello my name is John', 'h') //returns 2
+countLetters('We love this city a lot, sometimes', 'e') //returns 4
+
+const countLetter = (letters) => i + h + e
+console.log(letters(1)(2)(4))
